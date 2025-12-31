cd37dcdb0ff054ffb1cfd0454009787268460624
user <vitas02@list.ru>
2025-12-31 16:38:39 +0700

    time error

--- src/TimerProfiler.cpp
+++ src/TimerProfiler.cpp
@@ -6,9 +6,9 @@ void TimeProfiler::print(std::ostream &os)
 {
         os << "\n# === Time Profiling Results (ms) ===\n";
         os << std::fixed << std::setprecision(3);  // 3 знака после запятой
-        for (const auto& [name, time] : timings) {
-            os << "# " << std::setw(25) << std::left << name 
-                      << ": " << std::setw(10) << std::right << time << " ms\n";
+        for (const auto& it : timings) {
+            os << "# " << std::setw(25) << std::left << it.first
+                      << ": " << std::setw(10) << std::right << it.second << " ms\n";
         }
         os << std::resetiosflags(std::ios::fixed);  // Сброс форматирования
 }
